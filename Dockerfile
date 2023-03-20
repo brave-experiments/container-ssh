749a12e196a8fbc9655ef2bc358cb2db4cdc60c3
Ralph Giles <rgiles@brave.com>
2023-03-20 16:01:26 -0700

    Add nitriding framework
    Include a submodule for the nitriding framework and launch it
    in the container. This doesn't work with normal container runtimes
    but allows ssh into an AWS secure enclave when the `gvproxy` is
    also run on the host and set to forward traffic to the container's
    port 22.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ RUN apt-get update && apt-get install openssh-server sudo -y
 ADD start.sh /
 EXPOSE 22
 
+COPY nitriding /bin
+
 RUN useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test 
 RUN  echo 'test:test' | chpasswd
 
--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 
+# Start nitriding proxy
+nitriding -fqdn example.com -extport 8443 -intport 8081 &
+
+echo "Waiting for nitro to start up..."
+sleep 1
+
 # OpenSSH server needs a runtime chroot tree for login to work
 mkdir -p /run/sshd
 
